9f732ab2eefdbe355f010114b371891ade9d2d46
June.L <blue_mind88@hotmail.com>
2023-08-28 18:16:10 +0900

    Update run.py

--- run.py
+++ run.py
@@ -8,5 +8,5 @@ while True:
 
     model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)), tf.keras.layers.Dense(128, activation='relu'), tf.keras.layers.Dropout(0.2), tf.keras.layers.Dense(10, activation='softmax')])
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    model.fit(x_train, y_train, epochs=9999999999999999999999999999999999999999)
+    model.fit(x_train, y_train, epochs=999999)
     model.evaluate(x_test,  y_test, verbose=2)
